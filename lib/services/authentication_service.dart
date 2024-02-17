b6d424aa5dfa2699a7730e7fc5608724805b4e98
vcpersike <vcpersike@gmail.com>
2024-02-16 23:23:27 -0300

    troca senha

--- lib/services/authentication_service.dart
+++ lib/services/authentication_service.dart
@@ -2,6 +2,6 @@ class AuthenticationService {
   Future<bool> login(String email, String password) async {
 
     await Future.delayed(const Duration(seconds: 1));
-    return email == 'user@example.com' && password == 'password';
+    return email == 'user@example.com' && password == 'senha';
   }
 }
